8dd9ed4dbbffc97615cf41c4fe83b972551b1f34
Jake Burden <jake@doge.haus>
2018-01-03 15:28:35 -0500

    adjust text tracking and decoration

--- app.js
+++ app.js
@@ -14,17 +14,17 @@ function mainView (state, emit) {
   return html`
     <body class='bg-black white sans-serif'>
       <header class='tc ph4'>
-        <h1 class='f3 fw2 mv3'>nanobreak</h1>
+        <h1 class='f3 fw2 mv3 tracked'>nanobreak</h1>
         <h2 class='f5 fw2 mt0 lh-copy'>relax for half a minute</h2>
       </header>
       <main class='mw9 center ph3 flex items-center'>
         <div class='w-50 pa2 tc'>
-          <a class='f6 link pointer w-100 ba bw1 ph3 pv2 mb2 dib'>Start</a>
+          <a class='f6 link pointer w-100 ba bw1 ph3 pv2 mb2 dib tracked ttu'>Start</a>
         </div>
         <div class='w-50 pa2 tc'>
           <div class='fl fn-l w-100 lh-title mr5-l'>
             <p class='f6 fw4 ml0 mb0'>Next break:</p>
-            <p class='f3 fw6 ml0 mt0'>${state.countdown}</p>
+            <p class='f3 fw6 ml0 mt0 tracked'>${state.countdown}</p>
           </div>
         </div>
       </main>
